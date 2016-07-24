f91dd5ab44968ba0b4edb88896c71724e4f8c238
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2016-07-23 22:01:26 -0500

    Update client_ether_led_-_error_handshake.ino

--- client_ether_led_-_error_handshake.ino
+++ client_ether_led_-_error_handshake.ino
@@ -7,7 +7,7 @@
   byte datab[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
   //the value of the Indetification ID of the other client
-  char ValueOfId ='p';
+  char ValueOfId ='P';
   
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -45,6 +45,7 @@ void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   // give the Ethernet shield a second to initialize:
+  randomSeed(analogRead(0));
   delay(1000);
   Serial.println("connecting...");
 
@@ -108,8 +109,7 @@ int calcrc(char *ptr, int count)
     }
   if(readed == '1')
   {
-    digitalWrite(2, HIGH);// turn led White on to indicate the read of the sensor  
-    delay(1000);
+    digitalWrite(2, HIGH);// turn led White on to indicate the read of the sensor   
     // read the sensor and map the value
     i = analogRead(A0);
     i = map(i, 0, 1024, 0, 9);
@@ -151,14 +151,15 @@ int calcrc(char *ptr, int count)
         break;
     }
 
-
+    int randNumber = random(127);
+    data[7]=randNumber;
     Serial.print("Sensor: ");
     Serial.println(data);
     Serial.println("");
      for(int a=0;a<16;a++){
       datab[a]=data[a];
       } 
-    
+    delay(1000);
     digitalWrite(7, HIGH);// turn led Yellow on to indicate the calc of the CRC 
     
     unsigned short value = calcrc((char*)datab, 16);
@@ -206,5 +207,4 @@ int calcrc(char *ptr, int count)
       }
     return 1;
   }}
-
 
